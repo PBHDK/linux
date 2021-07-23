4a5ad5b720bb4d757f185ce21c852e704d7ed6d1
Jörg Thalheim <joerg@thalheim.io>
2021-07-23 22:19:52 +0000

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,45 +1,15 @@
-with import <nixpkgs> {
-  # crossSystem = {
-  #   config = "aarch64-unknown-linux-gnu";
-  # };
-};
-mkShell {
-  nativeBuildInputs = [
-    #pkgsCross.aarch64-multiplatform.binutils
-    pkgsCross.aarch64-multiplatform.buildPackages.gcc
-    getopt
-    flex
-    bison
-    binutils
-    #llvmPackages_11.llvm
-    clang_11
-    gnumake
-    bc
-    perl
-    hostname
-    cpio
-  ] ++ map lib.getDev [
-    elfutils
-    ncurses
-    openssl
-    zlib
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/953909341ea62291a4c4a9483214f9a12d58ec99.tar.gz") {} }:
+let
+  aarch64 = pkgs.pkgsCross.aarch64-multiplatform;
+in
+(aarch64.linux.override {
+  # To configure:
+  # make $makeFlags defconfig
+  # To build:
+  # make $makeFlags -j$(nproc)
+  stdenv = aarch64.clang11Stdenv;
+}).overrideAttrs (old: {
+  nativeBuildInputs = old.nativeBuildInputs ++ [
+    pkgs.gllvm
   ];
-  buildInputs = [
-  
-  ];
-}
-# bc
-# perl
-# binutils
-# bison
-# clang_11
-# cpio
-# flex
-# getopt
-# gnumake
-# git
-# hostname
-# elfutils
-# ncurses
-# openssl
-# zlib
+})
