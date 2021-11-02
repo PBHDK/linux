cff9b516bc5eab360482ad5f4cd8555c8278ad7f
PBDHK <paul.heidekrueger@tum.de>
2021-11-02 09:39:46 +0000

    add binutils 2.37

--- shell.nix
+++ shell.nix
@@ -2,16 +2,33 @@
 with import <nixpkgs> {};
 let
   aarch64 = pkgs.pkgsCross.aarch64-multiplatform;
+  binutils-unwrapped = aarch64.buildPackages.binutils-unwrapped.overrideAttrs (old: {
+    name = "binutils-2.37";
+    src = aarch64.fetchurl {
+      url = "https://ftp.gnu.org/gnu/binutils/binutils-2.37.tar.xz";
+      sha256 = "sha256-gg2XJPAgo+acszeJOgtjwtsWHa3LDgb8Edwp6x6Eoyw=";
+    };
+    patches = [];
+  });
+  cc = aarch64.buildPackages.wrapCCWith rec {
+    cc = aarch64.buildPackages.llvmPackages_11.clang-unwrapped;
+    bintools = aarch64.buildPackages.wrapBintoolsWith {
+      bintools = binutils-unwrapped;
+      libc = aarch64.glibc;
+    };
+  };
 in
 (aarch64.linux.override {
   # To configure:
   # make $makeFlags defconfig
   # To build:
   # make $makeFlags -j$(nproc)
-  stdenv = aarch64.clang11Stdenv;
+  stdenv = overrideCC aarch64.clang11Stdenv cc;
 }).overrideAttrs (old: {
   nativeBuildInputs = old.nativeBuildInputs ++ [
     pkgs.gllvm
+    #pkgs.llvmPackages_13.llvm
+    #pkgs.llvmPackages_13.bintools
     pkgs.ncurses
     pkgs.perl
   ];
