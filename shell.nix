9f4798aec3ab8d5e0cc0da9ac902fcdb5ed617c5
Paul Heidekrüger <paul.heidekrueger@in.tum.de>
2022-05-02 18:30:02 +0000

    Fix arm neon error

--- shell.nix
+++ shell.nix
@@ -1,21 +1,33 @@
 with import <nixpkgs> {};
 let
   aarch64 = pkgsCross.aarch64-multiplatform;
-  myclang = aarch64.buildPackages.wrapCC (stdenv.mkDerivation {
-    name = "impure-clang";                                                                          
-    dontUnpack = true;                                                                              
-    installPhase = ''                                                                               
-      mkdir -p $out/bin                                                                             
-      for bin in ${toString (builtins.attrNames (builtins.readDir /scratch/paul/src/llvm-project/build/bin))}; do
-        cat > $out/bin/$bin <<EOF
-      #!${runtimeShell}
-      exec "${toString /scratch/paul/src/llvm-project}/build/bin/$bin" "\$@"
-      EOF
-        chmod +x $out/bin/$bin
-      done
-    '';
-    passthru.isClang = true;
-  });
+  clangPath = toString ../llvm-project/build;
+  myclang = aarch64.buildPackages.wrapCCWith {
+    gccForLibs = null;
+    inherit (aarch64.buildPackages.targetPackages.llvmPackages_13.libraries) libcxx;
+    cc = (stdenv.mkDerivation {
+      name = "impure-clang";                                                                          
+      dontUnpack = true;                                                                              
+      installPhase = ''                                                                               
+        mkdir -p $out/bin                                                                             
+        for bin in ${toString (builtins.attrNames (builtins.readDir /scratch/paul/src/llvm-project/build/bin))}; do
+          cat > $out/bin/$bin <<EOF
+        #!${runtimeShell}
+        exec "${clangPath}/bin/$bin" "\$@"
+        EOF
+          chmod +x $out/bin/$bin
+        done
+      '';
+      passthru.isClang = true;
+    });
+  };
 in (aarch64.buildPackages.overrideCC aarch64.stdenv myclang).mkDerivation {
-    name = "env";
+  name = "env";
+  nativeBuildInputs = aarch64.linux.nativeBuildInputs;
+  depsBuildBuild = [
+    aarch64.buildPackages.stdenv.cc
+  ];
+  #NIX_CFLAGS_COMPILE = "-isystem ${clangPath}/build/tools/clang/lib/Headers";
+  NIX_LDFLAGS = "-L${aarch64.buildPackages.targetPackages.llvmPackages_13.libraries.libcxxabi}/lib";
+  hardeningDisable = [ "all" ];
 }
