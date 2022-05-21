9332d003a181805c6e896ab0292aab5a73b3f6c9
Paul Heidekrüger <paul.heidekrueger@in.tum.de>
2022-05-21 07:49:05 +0000

    Add structure for random testing script

---
+++ random_search_broken_deps.py
@@ -0,0 +1,17 @@
+# make mrproper
+
+# generate random config
+
+# make sure its a sensible config
+#   - arch?
+#   - enable debug symbols
+
+# build random config and attach to output
+
+# for every output: 
+#   check if we have already seen it
+#   if yes, discard
+#   if no, print it to file and add it to hashmap
+
+# if we have found at least one new broken dependency, save the corresponding
+# config somewhere
