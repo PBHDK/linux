13f62175d941a636e11b0d5443c086c130b6bef6
Paul Heidekrüger <paul.heidekrueger@in.tum.de>
2022-05-28 10:15:36 +0000

    Rename variables in build script

--- doitlk-linux-build.sh
+++ doitlk-linux-build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-makeFlags="CC=$CC ARCH=arm64 CROSS_COMPILE=aarch64-unknown-linux-gnu- KCFLAGS="-fsanitize=lkmm-dep-checker""
+ENABLE_DEP_CHEKER="-fsanitize=lkmm-dep-checker"
+ENABLE_TESTS="-mllvm -lkmm-enable-tests"
+PRINT_MODULES="-mllvm -debug-only=lkmm-print-modules"
+CROSS="ARCH=arm64 CROSS_COMPILE=aarch64-unknown-linux-gnu-"
+
+MAKEFLAGS="CC=clang $CROSS KCFLAGS=\"$ENABLE_DEP_CHEKER\""
+MAKEFLAGSTEST="CC=clang $CROSS KCFLAGS=\"$ENABLE_DEP_CHEKER $ENABLE_TESTS\"" 
+MAKEFLAGS_DEBUG_MODULES="CC=clang $CROSS KCFLAGS=\"$ENABLE_DEP_CHEKER $PRINT_MODULES\""
 
 case $1 in
 	"mrproper")
@@ -10,13 +17,16 @@ case $1 in
 		make clean
 		;;
 	"defconfig")
-		make $makeFlags defconfig
+		make $MAKEFLAGS defconfig
 		;;
 	"fast")	
-		make $makeFlags -j$(nproc) $2 2> build_output.ll
+		make $MAKEFLAGS -j$(nproc) $2 2> build_output.ll
 		;;
 	"precise")
-		make $makeFlags -j1 $2 2> build_output.ll
+		make $MAKEFLAGS -j1 $2 2> build_output.ll
+		;;
+	"tests")
+		make $MAKEFLAGSTEST -j1 lib/modules/dep_chain_tests.o 2> test_output.ll
 		;;
 	*)
 		echo "Invalid command line argument"
