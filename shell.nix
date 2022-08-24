c7774c449e9991c23c7e455e2d63413760b3b818
Paul Heidekrüger <paul.heidekrueger@in.tum.de>
2022-08-24 13:05:13 +0000

    Use python 3.11

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 let
   aarch64 = pkgsCross.aarch64-multiplatform;
   clangPath = toString ../llvm-project/build;
@@ -6,10 +6,10 @@ let
     gccForLibs = null;
     inherit (aarch64.buildPackages.targetPackages.llvmPackages_13.libraries) libcxx;
     cc = (stdenv.mkDerivation {
-      name = "impure-clang";                                                                          
-      dontUnpack = true;                                                                              
-      installPhase = ''                                                                               
-        mkdir -p $out/bin                                                                             
+      name = "impure-clang";
+      dontUnpack = true;
+      installPhase = ''
+        mkdir -p $out/bin
         for bin in ${toString (builtins.attrNames (builtins.readDir /scratch/paul/src/llvm-project/build/bin))}; do
           cat > $out/bin/$bin <<EOF
         #!${runtimeShell}
@@ -21,12 +21,14 @@ let
       passthru.isClang = true;
     });
   };
-in (aarch64.buildPackages.overrideCC aarch64.stdenv myclang).mkDerivation {
+in
+(aarch64.buildPackages.overrideCC aarch64.stdenv myclang).mkDerivation {
   name = "env";
   nativeBuildInputs = aarch64.linux.nativeBuildInputs;
   depsBuildBuild = [
     aarch64.buildPackages.stdenv.cc
     aarch64.buildPackages.ncurses
+    aarch64.buildPackages.python311
   ];
   #NIX_CFLAGS_COMPILE = "-isystem ${clangPath}/build/tools/clang/lib/Headers";
   NIX_LDFLAGS = "-L${aarch64.buildPackages.targetPackages.llvmPackages_13.libraries.libcxxabi}/lib";
