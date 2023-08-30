cdae53b0235a5ae1eaa0aac1c910b90aa394a2a1
Paul Heidekrüger <paul.heidekrueger@tum.de>
2023-08-30 20:25:17 +0000

    tests: re-introduce control dependencies
    Based on ecb6a309b74d02e60d6f8145c56f7405adc22844

--- proj_bdo/dep_chain_tests.c
+++ proj_bdo/dep_chain_tests.c
@@ -12,6 +12,8 @@ MODULE_LICENSE("GPL");
 #ifndef _PROJ_BDO_DEP_CHAIN_TESTS
 #define _PROJ_BDO_DEP_CHAIN_TESTS
 
+#define MAX 4242
+
 static int test_arr[10] = { 0x42424241, 0x42424242, 0x42424243, 0x42424244,
 			    0x42424245, 0x42424246, 0x42424247, 0x42424248,
 			    0x42424249, 0x4242424A };
@@ -853,6 +855,592 @@ static noinline int rw_addr_dep_end_beg_and_end_in_calls(void)
  * =============================================================================
  */
 
+/* 
+ * =============================================================================
+ * READ_ONCE() -> WRITE_ONCE() Control Dependencies 
+ * =============================================================================
+ */
+
+/* BUGs: 1 */
+/* Begin ctrl dep 1: memory-barriers.txt case 1, control dependency within the same function */
+static noinline int doitlk_ctrl_dep_begin_1(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	if (r1) {
+		r2 = &r1[4];
+
+		WRITE_ONCE(y, 42);
+	}
+
+	return 0;
+}
+
+/* BUGs: 1 */
+static noinline int doitlk_ctrl_dep_end_1(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	if (r1) {
+		r2 = &r1[4];
+
+		WRITE_ONCE(y, 42);
+	}
+
+	return 0;
+}
+
+/* BUGs: 0 */
+static noinline int doitlk_ctrl_dep_begin_2(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	if (x[4]) {
+		r2 = &r1[8];
+
+		WRITE_ONCE(y, *r2);
+	}
+
+	return 0;
+}
+
+/* BUGs: 0 */
+static noinline int doitlk_ctrl_dep_end_2(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	if (x[4]) {
+		r2 = &r1[8];
+
+		WRITE_ONCE(y, *r2);
+	}
+
+	return 0;
+}
+
+/* BUGs: 0 */
+static noinline int doitlk_ctrl_dep_begin_3(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	if (0) {
+		r2 = &r1[42];
+
+		WRITE_ONCE(y, 44);
+	}
+
+	return 0;
+}
+
+/* BUGs: 0 */
+static noinline int doitlk_ctrl_dep_end_3(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	if (0) {
+		r2 = &r1[42];
+
+		WRITE_ONCE(y, 44);
+	}
+
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_begin_4(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+	if (r1) {
+		r2 = &r1[6];
+
+		WRITE_ONCE(*r2, 42);
+	}
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_4(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+	if (r1) {
+		r2 = &r1[6];
+
+		WRITE_ONCE(*r2, 42);
+	}
+	return 0;
+}
+
+// Begin ctrl dep 5: control dependency with end in for loop -  for breaking the begin annotation
+static noinline int doitlk_ctrl_dep_begin_5(void)
+{
+	int i = 0;
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	for (; i < 10; ++i) {
+		r2 = &r1[i];
+
+		WRITE_ONCE(*r2, 1);
+	}
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_5(void)
+{
+	int i = 0;
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	for (; i < 10; ++i) {
+		r2 = &r1[i];
+
+		WRITE_ONCE(*r2, 1);
+	}
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_begin_6(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+	if (*r1 % MAX == 0)
+		WRITE_ONCE(*r1, y);
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_6(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+	if (*r1 % MAX == 0)
+		WRITE_ONCE(*r1, y);
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_begin_7(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+	if (r1 || 1 > 0)
+		WRITE_ONCE(y, 1);
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_7(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+	if (r1 || 1 > 0)
+		WRITE_ONCE(y, 1);
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_begin_8(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+	if (*r1 > 0) {
+		if (*r1 > 42)
+			WRITE_ONCE(y, 42);
+		else
+			WRITE_ONCE(y, 0);
+	}
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_8(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+	if (*r1 > 0) {
+		if (*r1 > 42)
+			WRITE_ONCE(y, 42);
+		else
+			WRITE_ONCE(y, 0);
+	}
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_begin_9(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*(x + 4));
+	r2 = READ_ONCE(*x);
+	if (*r1 > *r2)
+		WRITE_ONCE(y, 0);
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_9(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*(x + 4));
+	r2 = READ_ONCE(*x);
+	if (*r1 > *r2)
+		WRITE_ONCE(y, 0);
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_begin_10(void)
+{
+	int *r1;
+	int *r2;
+	int *r3;
+
+	r1 = READ_ONCE(*(x + 1));
+	r2 = READ_ONCE(*(x + 2));
+	r3 = READ_ONCE(*(x + 3));
+
+	if (r1 > r2 || r3 < r1)
+		WRITE_ONCE(y, 1);
+
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_10(void)
+{
+	int *r1;
+	int *r2;
+	int *r3;
+
+	r1 = READ_ONCE(*(x + 1));
+	r2 = READ_ONCE(*(x + 2));
+	r3 = READ_ONCE(*(x + 3));
+
+	if (r1 > r2 || r3 < r1)
+		WRITE_ONCE(y, 1);
+
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_begin_11(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+	if (*r1) {
+		r2 = READ_ONCE(*(x + 9));
+		if (*r2)
+			WRITE_ONCE(*r2, 1);
+	}
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_11(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+	if (*r1) {
+		r2 = READ_ONCE(*(x + 9));
+		if (*r2)
+			WRITE_ONCE(*r2, 1);
+	}
+	return 0;
+}
+
+static noinline int ctrl_dep_12_begin_helper(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+	if (r1)
+		return 21;
+	else
+		return 42;
+}
+
+static noinline int doitlk_ctrl_dep_begin_12(void)
+{
+	WRITE_ONCE(y, ctrl_dep_12_begin_helper());
+	return 0;
+}
+
+static noinline int ctrl_dep_12_end_helper(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+	if (r1)
+		return 21;
+	else
+		return 42;
+}
+
+static noinline int doitlk_ctrl_dep_end_12(void)
+{
+	WRITE_ONCE(y, ctrl_dep_12_end_helper());
+	return 0;
+}
+
+static int *noinline ctrl_dep_begin_13_helper(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	r2 = &r1[4];
+
+	return r2;
+}
+
+static noinline int doitlk_ctrl_dep_begin_13(void)
+{
+	int *r1;
+
+	r1 = ctrl_dep_begin_13_helper();
+	if (r1)
+		WRITE_ONCE(*r1, 1);
+
+	return 0;
+}
+
+static int *noinline ctrl_dep_end_13_helper(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	r2 = &r1[4];
+
+	return r2;
+}
+
+static noinline int doitlk_ctrl_dep_end_13(void)
+{
+	int *r1;
+
+	r1 = ctrl_dep_end_13_helper();
+
+	if (r1)
+		WRITE_ONCE(*r1, 1);
+
+	return 0;
+}
+
+static int *noinline ctrl_dep_begin_14_helper1(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	r2 = &r1[6];
+
+	return r2;
+}
+
+static noinline void ctrl_dep_begin_14_helper2(int *r1)
+{
+	WRITE_ONCE(*r1, y);
+}
+
+static noinline int doitlk_ctrl_dep_begin_14(void)
+{
+	int *r1;
+
+	r1 = ctrl_dep_begin_14_helper1();
+	if (r1)
+		ctrl_dep_begin_14_helper2(r1);
+
+	return 0;
+}
+
+static int *noinline ctrl_dep_end_14_helper1(void)
+{
+	int *r1;
+	int *r2;
+
+	r1 = READ_ONCE(*x);
+
+	r2 = &r1[6];
+
+	return r2;
+}
+
+static noinline void ctrl_dep_end_14_helper2(int *r1)
+{
+	WRITE_ONCE(*r1, y);
+}
+
+static noinline int doitlk_ctrl_dep_end_14(void)
+{
+	int *r1;
+
+	r1 = ctrl_dep_end_14_helper1();
+
+	if (r1)
+		ctrl_dep_end_14_helper2(r1);
+
+	return 0;
+}
+
+static noinline void ctrl_dep_begin_15_helper(int *r1)
+{
+	WRITE_ONCE(*r1, y);
+}
+
+static noinline int doitlk_ctrl_dep_begin_15(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+
+	if (r1)
+		ctrl_dep_begin_15_helper(r1);
+
+	return 0;
+}
+
+static noinline int doitlk_ctrl_dep_end_15(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+
+	if (r1)
+		ctrl_dep_begin_15_helper(r1);
+
+	return 0;
+}
+
+static noinline void ctrl_dep_end_15_helper(int *r1)
+{
+	WRITE_ONCE(*r1, y);
+}
+
+static noinline int doitlk_ctrl_dep_begin_16(void)
+{
+	int *r1;
+loop:
+	r1 = READ_ONCE(*x);
+
+	if (r1)
+		goto end;
+
+	// do something with x
+	x++;
+	goto loop;
+
+end:
+	WRITE_ONCE(y, *r1);
+
+	return *r1;
+}
+
+static noinline int doitlk_ctrl_dep_end_16(void)
+{
+	int *r1;
+loop:
+	r1 = READ_ONCE(*x);
+
+	if (r1)
+		goto end;
+
+	// do something with x
+	x++;
+	goto loop;
+
+end:
+	WRITE_ONCE(y, *r1);
+
+	return *r1;
+}
+
+static noinline int doitlk_ctrl_dep_begin_17(void)
+{
+	int *r1;
+loop:
+	r1 = READ_ONCE(*x);
+
+	if (*r1 == *x[4]) {
+		WRITE_ONCE(y, *r1);
+		return *r1;
+	}
+
+	*r1 += 1;
+	goto loop;
+}
+
+static noinline int doitlk_ctrl_dep_end_17(void)
+{
+	int *r1;
+loop:
+	r1 = READ_ONCE(*x);
+
+	if (*r1 == *x[4]) {
+		WRITE_ONCE(y, *r1);
+		return *r1;
+	}
+
+	*r1 += 1;
+	goto loop;
+}
+
+// Begin ctrl dep 18: memory-barriers.txt case 2, constant dependent condition
+static noinline int doitlk_ctrl_dep_begin_18(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+
+	if (*r1 % MAX == 0)
+		WRITE_ONCE(*r1, y);
+	return 0;
+}
+
+// End ctrl dep 18: memory-barriers.txt case 2, constant dependent condition
+static noinline int doitlk_ctrl_dep_end_18(void)
+{
+	int *r1;
+
+	r1 = READ_ONCE(*x);
+
+	if (*r1 % MAX == 0)
+		WRITE_ONCE(*r1, y);
+	return 0;
+}
+
 static void noinline initialise_test_data(void)
 {
 	for (int i = 0; i < 10; ++i)
@@ -918,6 +1506,44 @@ int proj_bdo_run_tests(void)
 
 	rw_addr_dep_begin_beg_and_end_in_calls();
 	rw_addr_dep_end_beg_and_end_in_calls();
+
+	/* Ctrl dependencies */
+	doitlk_ctrl_dep_begin_1();
+	doitlk_ctrl_dep_end_1();
+	doitlk_ctrl_dep_begin_2();
+	doitlk_ctrl_dep_end_2();
+	doitlk_ctrl_dep_begin_3();
+	doitlk_ctrl_dep_end_3();
+	doitlk_ctrl_dep_begin_4();
+	doitlk_ctrl_dep_end_4();
+	doitlk_ctrl_dep_begin_5();
+	doitlk_ctrl_dep_end_5();
+	doitlk_ctrl_dep_begin_6();
+	doitlk_ctrl_dep_end_6();
+	doitlk_ctrl_dep_begin_7();
+	doitlk_ctrl_dep_end_7();
+	doitlk_ctrl_dep_begin_8();
+	doitlk_ctrl_dep_end_8();
+	doitlk_ctrl_dep_begin_9();
+	doitlk_ctrl_dep_end_9();
+	doitlk_ctrl_dep_begin_10();
+	doitlk_ctrl_dep_end_10();
+	doitlk_ctrl_dep_begin_11();
+	doitlk_ctrl_dep_end_11();
+	doitlk_ctrl_dep_begin_12();
+	doitlk_ctrl_dep_end_12();
+	doitlk_ctrl_dep_begin_13();
+	doitlk_ctrl_dep_end_13();
+	doitlk_ctrl_dep_begin_14();
+	doitlk_ctrl_dep_end_14();
+	doitlk_ctrl_dep_begin_15();
+	doitlk_ctrl_dep_end_15();
+	doitlk_ctrl_dep_begin_16();
+	doitlk_ctrl_dep_end_16();
+	doitlk_ctrl_dep_begin_17();
+	doitlk_ctrl_dep_end_17();
+	doitlk_ctrl_dep_begin_18();
+	doitlk_ctrl_dep_end_18();
 
 	return 0;
 }
