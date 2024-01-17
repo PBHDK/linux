cb354dccbb8701585e5a7ca872ee9c48c400a64e
Paul Heidekrüger <paul.heidekrueger@tum.de>
2024-01-17 14:03:45 +0000

    formatting

--- proj_bdo/common/utils.py
+++ proj_bdo/common/utils.py
@@ -1,13 +1,17 @@
 """Various utiliteis required for the dep checker."""
-import subprocess
 import os
+import subprocess
+from typing import Optional, TextIO
 
-from typing import TextIO, Optional
+_CLANG_FLAGS = [
+    "HOSTCC=gcc",
+    "CC=clang",
+]
 
-_CLANG_FLAGS = ["HOSTCC=gcc", "CC=clang",]
-
-_CLANG_ARM64_ENV = _CLANG_FLAGS + \
-    ["ARCH=arm64", "CROSS_COMPILE=aarch64-unknown-linux-gnu-"]
+_CLANG_ARM64_ENV = _CLANG_FLAGS + [
+    "ARCH=arm64",
+    "CROSS_COMPILE=aarch64-unknown-linux-gnu-",
+]
 _CLANG_X86_64_ENV = _CLANG_FLAGS + ["ARCH=x86_64"]
 
 
@@ -24,12 +28,14 @@ def count_str_file(s: str, f: TextIO):
     return fstr.count(s)
 
 
-def run(args: list[str],
-        stderr: TextIO = None,
-        stdout: TextIO = None,
-        shell: bool = False,
-        text: bool = True,
-        cwd: Optional[str] = None):
+def run(
+    args: list[str],
+    stderr: TextIO = None,
+    stdout: TextIO = None,
+    shell: bool = False,
+    text: bool = True,
+    cwd: Optional[str] = None,
+):
     """
     Run a shell command and print it to stdout.
 
@@ -44,12 +50,9 @@ def run(args: list[str],
     """
     print("[ " + " ".join(args) + " ]\n")
 
-    return subprocess.run(args=args,
-                          stderr=stderr,
-                          stdout=stdout,
-                          shell=shell,
-                          text=text,
-                          cwd=cwd)
+    return subprocess.run(
+        args=args, stderr=stderr, stdout=stdout, shell=shell, text=text, cwd=cwd
+    )
 
 
 def clean_kernel():
@@ -64,9 +67,9 @@ def mrproper_kernel():
 
 def get_kernel_version():
     """Get the kernel version of HEAD in the kernel repo."""
-    return run(args=["make", "kernelversion"],
-               stdout=subprocess.PIPE,
-               text=True).stdout.strip()
+    return run(
+        args=["make", "kernelversion"], stdout=subprocess.PIPE, text=True
+    ).stdout.strip()
 
 
 def get_dep_checker_ver() -> str:
@@ -76,14 +79,17 @@ def get_dep_checker_ver() -> str:
 
     returnss: the dep checker version as a string.
     """
-    branch: str = run(["git", "rev-parse", "--abbrev-ref", "HEAD"],
-                      cwd=os.getenv("PROJECTS") + "/llvm-project",
-                      stdout=subprocess.PIPE).stdout.rstrip("\n")
+    branch: str = run(
+        ["git", "rev-parse", "--abbrev-ref", "HEAD"],
+        cwd=os.getenv("PROJECTS") + "/llvm-project",
+        stdout=subprocess.PIPE,
+    ).stdout.rstrip("\n")
 
     head: str = run(
         ["git", "--no-pager", "show", "HEAD", "--oneline", "-s"],
         cwd=os.getenv("PROJECTS") + "/llvm-project",
-        stdout=subprocess.PIPE).stdout.rstrip("\n")
+        stdout=subprocess.PIPE,
+    ).stdout.rstrip("\n")
 
     return (branch + "-" + head).replace(" ", "-")
 
@@ -91,26 +97,26 @@ def get_dep_checker_ver() -> str:
 def add_dep_checker_support_to_current_config():
     """Set all options the dep checker requires in a kernel config."""
     run(["./scripts/config", "--enable", "CONFIG_DEBUG_INFO"])
-    run(["./scripts/config", "--enable",
-         "CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT"])
     run(["./scripts/config", "--disable", "CONFIG_DEBUG_INFO_REDUCED"])
     run(["./scripts/config", "--enable", "DEBUG_INFO_COMPRESSED_NONE"])
     run(["./scripts/config", "--disable", "DEBUG_INFO_COMPRESSED_ZLIB"])
     run(["./scripts/config", "--disable", "CONFIG_DEBUG_INFO_SPLIT"])
     run(["./scripts/config", "--enable", "CONFIG_GDB_SCRIPTS"])
+    run(["./scripts/config", "--enable", "CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT"])
     run(["./scripts/config", "--enable", "DEBUG_KERNEL"])
     run(["./scripts/config", "--enable", "LKMMDC"])
     run(["./scripts/config", "--disable", "LKMMDC_TEST"])
 
 
-def add_syzkaller_support_to_config(arch: str,add_args: list[str]=[]):
+def add_syzkaller_support_to_config(arch: str, add_args: list[str] = []):
     return
     # Suggested by:
     # https://docs.kernel.org/dev-tools/gdb-kernel-debugging.html
     print("\nBuilding GDB Scripts:\n")
     run(["./scripts/config", "--enable", "CONFIG_GDB_SCRIPTS"])
-    clang_build_kernel(ObjPath="scripts_gdb",
-                       add_args=add_args, stderr="/dev/null", arch=arch)
+    clang_build_kernel(
+        ObjPath="scripts_gdb", add_args=add_args, stderr="/dev/null", arch=arch
+    )
 
     # Suggested by:
     # https://github.com/google/syzkaller/blob/master/docs/linux/setup_linux-host_qemu-vm_arm64-kernel.md
@@ -123,8 +129,7 @@ def add_syzkaller_support_to_config(arch: str,add_args: list[str]=[]):
     run(["./scripts/config", "--enable", "CONFIG_DEBUG_FS"])
     run(["./scripts/config", "--enable", "CONFIG_NET_9P"])
     run(["./scripts/config", "--enable", "CONFIG_NET_9P_VIRTIO"])
-    run(["./scripts/config", "--set-str",
-        "CONFIG_CROSS_COMPILE", "aarch64-linux-gnu-"])
+    run(["./scripts/config", "--set-str", "CONFIG_CROSS_COMPILE", "aarch64-linux-gnu-"])
 
 
 def configure_kernel(config: str, add_args: list[str] = [], arch: str = "arm64"):
@@ -142,11 +147,13 @@ def configure_kernel(config: str, add_args: list[str] = [], arch: str = "arm64")
     add_dep_checker_support_to_current_config()
 
 
-def clang_build_kernel(add_args: list[str] = list(),
-                       threads=os.getenv("NIX_BUILD_CORES", "128"),
-                       ObjPath="",
-                       stderr="build_output.err",
-                       arch="arm64"):
+def clang_build_kernel(
+    add_args: list[str] = list(),
+    threads=os.getenv("NIX_BUILD_CORES", "128"),
+    ObjPath="",
+    stderr="build_output.err",
+    arch="arm64",
+):
     """
     Build an arm64 Linux kernel with the DepChecker support enabled.
 
@@ -165,25 +172,31 @@ def clang_build_kernel(add_args: list[str] = list(),
         res = None
 
         if ObjPath:
-            if (os.path.exists(ObjPath)):
+            if os.path.exists(ObjPath):
                 run(["rm"] + [ObjPath], stderr=SE)
-            if (ObjPath == "proj_bdo/dep_chain_tests.o"):
+            if ObjPath == "proj_bdo/dep_chain_tests.o":
                 res = run(
-                    ["/usr/bin/time", "-v", "-o", "/dev/stdout", "make"] +
-                    env + add_args +
-                    [JStr, ObjPath, "-s"], stderr=SE
+                    ["/usr/bin/time", "-v", "-o", "/dev/stdout", "make"]
+                    + env
+                    + add_args
+                    + [JStr, ObjPath, "-s"],
+                    stderr=SE,
                 )
             else:
                 res = run(
-                    ["/usr/bin/time", "-v", "-o", "/dev/stdout", "make"] +
-                    env + add_args +
-                    [JStr, ObjPath, "-s"], stderr=SE
+                    ["/usr/bin/time", "-v", "-o", "/dev/stdout", "make"]
+                    + env
+                    + add_args
+                    + [JStr, ObjPath, "-s"],
+                    stderr=SE,
                 )
         else:
             res = run(
-                ["/usr/bin/time", "-v", "-o", "/dev/stdout", "make"] +
-                env + add_args + [JStr, "-s"],
-                stderr=SE
+                ["/usr/bin/time", "-v", "-o", "/dev/stdout", "make"]
+                + env
+                + add_args
+                + [JStr, "-s"],
+                stderr=SE,
             )
 
     print("\nGenerating compilation database:\n")
